d7a4aa42d4cdbc78e42e06da88093cbe804b6242
fillmello <fillmello@outlook.com>
2025-07-14 02:36:52 -0300

    Etapa 1 - Framework Express

--- ETAPA 1/Módulos/1- Hands On/index.js
+++ ETAPA 1/Módulos/1- Hands On/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const morgan = require ('morgan');
 const port = 4000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('dev')); //ja inclui o next()
+
+app.use('/site', express.static('public'));
 
 app.get('/', (req,res) => {
     res.send('Hello World!');
+});
+
+app.use((req,res) => {
+    res.status (404).send('404 Not Found');   
 });
 
 app.listen(port, () => {
